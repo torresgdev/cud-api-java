464bb997b86873df9a1a44d6aaa5ed9b32ace415
Gabriel Torres <torresg.dev@gmail.com>
2025-08-06 08:14:51 -0300

    Starting project.Feat(model): add Product

---
+++ src/main/java/com/project/api_crud/model/Product.java
@@ -0,0 +1,48 @@
+package com.project.api_crud.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(nullable = false, length = 100)
+    private String description;
+
+    @Column(nullable = false)
+    private Double price;
+
+    @Column(nullable = false)
+    private Integer stock;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected  void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
---
+++ src/test/java/com/project/api_crud/ApiCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.api_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
