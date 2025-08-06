ab3348d73cf1db28019c13db6f43d3ee56367192
Gabriel Torres <torresg.dev@gmail.com>
2025-08-06 15:02:43 -0300

    Feat(repository): update ProductRepository

--- src/main/java/com/project/api_crud/repository/ProductRepository.java
+++ src/main/java/com/project/api_crud/repository/ProductRepository.java
@@ -2,11 +2,13 @@ package com.project.api_crud.repository;
 
 import com.project.api_crud.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.math.BigDecimal;
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     Optional<Product> findByName(String name);
 
-    Optional<Product> findByPrice(Integer price);
+    Optional<Product> findByPrice(BigDecimal price);
 }
