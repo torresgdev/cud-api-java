e421de6cf1d8bccb2c9c87da83d866174af2197f
Gabriel Torres <torresg.dev@gmail.com>
2025-08-06 08:39:30 -0300

    Feat(dto): add ProductCreateDTO

--- src/main/java/com/project/api_crud/dto/ProductCreateDTO.java
+++ src/main/java/com/project/api_crud/dto/ProductCreateDTO.java
@@ -1,6 +1,33 @@
 package com.project.api_crud.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductCreateDTO {
+
+    @NotBlank(message = "O nome do produto é obrigatório")
+    @Size(message = "O nome do produto não pode ultrapassar de 50 caracteres")
+    private String name;
+
+    @NotBlank(message = "A descrição do produto é obrigatória")
+    @Size(message = "A descrição do produto não pode ultrapassar de 100 caracteres")
+    private String description;
+
+    @NotNull(message = "O preço do produto é obrigatório")
+    @Positive(message = "Preço do produto deve ser maior que 0")
+    private Double price;
+
+    @NotNull(message = "A Quantidade do produto é obrigatória")
+    @Positive(message = "Stock do produto não pode ser 0")
+    private Integer stock;
 }
--- src/main/java/com/project/api_crud/model/Product.java
+++ src/main/java/com/project/api_crud/model/Product.java
@@ -25,7 +25,7 @@ public class Product {
     @Column(nullable = false, length = 100)
     private String description;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 20)
     private Double price;
 
     @Column(nullable = false)
